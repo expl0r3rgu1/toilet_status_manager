3866b392005dd7e2042012c3e9c251bfa6a797b7
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-22 01:54:37 +0200

    Created onVerificationCompleted and checkSmsCodeAndSignIn methods in FirebaseAuthenticationServices

--- lib/firebase/firebase_authentication_services.dart
+++ lib/firebase/firebase_authentication_services.dart
@@ -4,74 +4,44 @@ import 'package:toilet_status_manager/home_page.dart';
 
 class FirebaseAuthenticationServices {
   static const int smsCodeTimeout = 60;
+  static FirebaseAuth auth = FirebaseAuth.instance;
 
-  static Future registerUser(String mobile, BuildContext context) async {
-    FirebaseAuth auth = FirebaseAuth.instance;
+  static Future<bool> onVerificationCompleted(
+      PhoneAuthCredential credential, BuildContext context) async {
+    try {
+      await auth
+          .signInWithCredential(credential)
+          .then((UserCredential userCredential) {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomePage(userCredential.user!)));
+      });
+    } catch (e) {
+      return false;
+    }
 
-    auth.verifyPhoneNumber(
-        phoneNumber: mobile,
-        timeout: const Duration(seconds: smsCodeTimeout),
-        verificationCompleted: (PhoneAuthCredential credential) async {
-          await auth
-              .signInWithCredential(credential)
-              .then((UserCredential userCredential) {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => HomePage(userCredential.user!)));
-          }).catchError((e) {
-            print(e);
-          });
-        },
-        verificationFailed: (FirebaseAuthException authException) {
-          print(authException.message);
-        },
-        codeSent: (String verificationId, int? forceResendingToken) {
-          TextEditingController smsCodeController = TextEditingController();
-          showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (context) => AlertDialog(
-                    title: const Text("Enter SMS Code"),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        TextField(
-                          controller: smsCodeController,
-                        ),
-                      ],
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text("Done"),
-                        onPressed: () {
-                          String smsCode = smsCodeController.text.trim();
+    return true;
+  }
 
-                          PhoneAuthCredential phoneAuthCredential =
-                              PhoneAuthProvider.credential(
-                                  verificationId: verificationId,
-                                  smsCode: smsCode);
+  static Future<bool> checkSmsCodeAndSignIn(String verificationId,
+      String smsCode, int? forceResendingToken, BuildContext context) async {
+    try {
+      PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: smsCode);
 
-                          auth
-                              .signInWithCredential(phoneAuthCredential)
-                              .then((UserCredential userCredential) {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        HomePage(userCredential.user!)));
-                          }).catchError((e) {
-                            print(e);
-                          });
-                        },
-                      )
-                    ],
-                  ));
-        },
-        codeAutoRetrievalTimeout: (String verificationId) {
-          verificationId = verificationId;
-          print(verificationId);
-          print("Timout");
-        });
+      await FirebaseAuthenticationServices.auth
+          .signInWithCredential(phoneAuthCredential)
+          .then((UserCredential userCredential) {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomePage(userCredential.user!)));
+      });
+    } catch (e) {
+      return false;
+    }
+
+    return true;
   }
 }
