e5f68715aadae55aa4c93b48ef3083e5cb62cfda
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-21 21:42:42 +0200

    Added final user field to HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final User user;
+  const HomePage(this.user, {Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
