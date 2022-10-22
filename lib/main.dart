7b5714f19fcee871a5795b4d9bca88c9d9f28d99
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-22 23:26:31 +0200

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: (FirebaseAuth.instance.currentUser != null)
           ? HomePage(
               FirebaseAuth.instance.currentUser!,
