fa4118634e053001c7847800637d16770e76aeb7
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-22 13:13:11 +0200

    Created methods getToiletId, getToilet, createUser, checkIfUserExists and createToilet

---
+++ lib/firebase/firestore_services.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:toilet_status_manager/model/toilet.dart';
+
+class FirestoreServices {
+  final CollectionReference _usersCollectionReference =
+      FirebaseFirestore.instance.collection('users');
+  final CollectionReference _toiletsCollectionReference =
+      FirebaseFirestore.instance.collection('toilets');
+
+  Future<String> getToiletId(String uid) async {
+    final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await _usersCollectionReference.doc(uid).get()
+            as DocumentSnapshot<Map<String, dynamic>>;
+    return documentSnapshot.data()!['toilet_id'];
+  }
+
+  Future<Toilet?> getToilet(String uid) async {
+    return await getToiletId(uid).then((toiletId) async {
+      if (toiletId.isEmpty) {
+        return null;
+      }
+      final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+          await _toiletsCollectionReference.doc(toiletId).get()
+              as DocumentSnapshot<Map<String, dynamic>>;
+      return Toilet(
+        uid: documentSnapshot.id,
+        nickname: documentSnapshot.data()!['nickname'],
+      );
+    });
+  }
+
+  Future<void> createUser(String uid) async {
+    await _usersCollectionReference.doc(uid).set({'toilet_id': ''});
+  }
+
+  Future<bool> checkIfUserExists(String uid) async {
+    final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await _usersCollectionReference.doc(uid).get()
+            as DocumentSnapshot<Map<String, dynamic>>;
+    return documentSnapshot.exists;
+  }
+
+  Future<void> createToilet(String uid, String nickname) async {
+    final DocumentReference documentReference =
+        await _toiletsCollectionReference
+            .add({'nickname': nickname, 'status': false});
+    await _usersCollectionReference.doc(uid).update({
+      'toilet_id': documentReference.id,
+    });
+  }
+}
