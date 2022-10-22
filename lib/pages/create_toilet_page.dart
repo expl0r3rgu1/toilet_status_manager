f01a8a7792f2be90345d3c26f4452dbdd084f852
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-22 13:14:11 +0200

    Created CreateToiletPage

---
+++ lib/pages/create_toilet_page.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:toilet_status_manager/firebase/firestore_services.dart';
+
+class CreateToiletPage extends StatefulWidget {
+  final User user;
+  const CreateToiletPage(this.user, {super.key});
+
+  @override
+  State<CreateToiletPage> createState() => _CreateToiletPageState();
+}
+
+class _CreateToiletPageState extends State<CreateToiletPage> {
+  final _toiletNameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Toilet Name",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _toiletNameController,
+                      decoration: InputDecoration(
+                        hintText: "Toilet Name",
+                        hintStyle: Theme.of(context).textTheme.bodyText1,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.03,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        FirestoreServices firestoreServices =
+                            FirestoreServices();
+                        await firestoreServices
+                            .createToilet(widget.user.uid,
+                                _toiletNameController.text.trim())
+                            .then((value) => Navigator.pop(context, () {
+                                  setState(() {});
+                                }));
+                      },
+                      child: Text(
+                        "Create",
+                        style: Theme.of(context).textTheme.headline5,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text(
+                    "Cancel",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
