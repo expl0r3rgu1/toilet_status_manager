fced9710d2586c2f2dbdc375b862bc421a81249e
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-23 19:37:38 +0200

    Set up firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:json_theme/json_theme.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +12,8 @@ import 'package:toilet_status_manager/login_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   await analytics.logAppOpen();
