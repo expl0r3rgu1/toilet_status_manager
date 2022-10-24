f61c8b05f49fc6daa56e626b293df51da8cf4d76
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-10-24 23:52:30 +0200

    Added form validator for phone number and sms code input
    Closes #6

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,11 +12,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _phoneFormKey = GlobalKey<FormState>();
+  final _codeFormKey = GlobalKey<FormState>();
   final _phoneNumberController = TextEditingController();
   final _smsCodeController = TextEditingController();
   bool codeSent = false;
   String? verificationId;
   int? forceResendingToken;
+  bool phoneValid = true;
 
   @override
   Widget build(BuildContext context) {
@@ -43,65 +46,87 @@ class _LoginPageState extends State<LoginPage> {
                       SizedBox(
                         height: MediaQuery.of(context).size.height * 0.03,
                       ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              controller: _smsCodeController,
-                              keyboardType: TextInputType.number,
-                              decoration: InputDecoration(
-                                hintText: "SMS Code",
-                                hintStyle:
-                                    Theme.of(context).textTheme.bodyText1,
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
+                      Form(
+                        key: _codeFormKey,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                controller: _smsCodeController,
+                                keyboardType: TextInputType.number,
+                                autofocus: true,
+                                validator: (value) {
+                                  //regex for 6 digit number
+                                  if (value!.isEmpty) {
+                                    return "Please enter the code";
+                                  } else if (!RegExp(r"^[0-9]{6}$")
+                                      .hasMatch(value)) {
+                                    return "Please enter a valid code";
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "SMS Code",
+                                  hintStyle:
+                                      Theme.of(context).textTheme.bodyText1,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  errorBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Colors.red,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.03,
-                          ),
-                          ElevatedButton(
-                            style: ButtonStyle(
-                              fixedSize: MaterialStateProperty.all(
-                                Size(
-                                  MediaQuery.of(context).size.width * 0.28,
-                                  MediaQuery.of(context).size.height * 0.05,
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.03,
+                            ),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                fixedSize: MaterialStateProperty.all(
+                                  Size(
+                                    MediaQuery.of(context).size.width * 0.28,
+                                    MediaQuery.of(context).size.height * 0.05,
+                                  ),
                                 ),
                               ),
-                            ),
-                            onPressed: () async {
-                              await FirebaseAuthenticationServices
-                                      .checkSmsCodeAndSignIn(
-                                          verificationId!,
-                                          _smsCodeController.text.trim(),
-                                          forceResendingToken,
-                                          context)
-                                  .then((value) {
-                                if (value) {
-                                  Fluttertoast.showToast(
-                                      msg: "Login successful",
-                                      toastLength: Toast.LENGTH_SHORT,
-                                      gravity: ToastGravity.BOTTOM,
-                                      timeInSecForIosWeb: 1,
-                                      fontSize: 16.0);
-                                } else {
-                                  Fluttertoast.showToast(
-                                      msg: "Login failed",
-                                      toastLength: Toast.LENGTH_SHORT,
-                                      gravity: ToastGravity.BOTTOM,
-                                      timeInSecForIosWeb: 1,
-                                      fontSize: 16.0);
+                              onPressed: () async {
+                                if (_codeFormKey.currentState!.validate()) {
+                                  await FirebaseAuthenticationServices
+                                          .checkSmsCodeAndSignIn(
+                                              verificationId!,
+                                              _smsCodeController.text.trim(),
+                                              forceResendingToken,
+                                              context)
+                                      .then((value) {
+                                    if (value) {
+                                      Fluttertoast.showToast(
+                                          msg: "Login successful",
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.BOTTOM,
+                                          timeInSecForIosWeb: 1,
+                                          fontSize: 16.0);
+                                    } else {
+                                      Fluttertoast.showToast(
+                                          msg: "Login failed",
+                                          toastLength: Toast.LENGTH_SHORT,
+                                          gravity: ToastGravity.BOTTOM,
+                                          timeInSecForIosWeb: 1,
+                                          fontSize: 16.0);
+                                    }
+                                  });
                                 }
-                              });
-                            },
-                            child: Text(
-                              "Done",
-                              style: Theme.of(context).textTheme.bodyText1,
+                              },
+                              child: Text(
+                                "Done",
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       )
                     ],
                   )
@@ -138,36 +163,59 @@ class _LoginPageState extends State<LoginPage> {
                       SizedBox(
                         height: MediaQuery.of(context).size.height * 0.03,
                       ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              controller: _phoneNumberController,
-                              keyboardType: TextInputType.phone,
-                              decoration: InputDecoration(
-                                hintText: "Phone Number",
-                                hintStyle:
-                                    Theme.of(context).textTheme.bodyText1,
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
+                      Form(
+                        key: _phoneFormKey,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                controller: _phoneNumberController,
+                                keyboardType: TextInputType.phone,
+                                autofocus: true,
+                                validator: (value) {
+                                  RegExp regex =
+                                      RegExp(r"^\+[1-9]{1}[0-9]{3,14}$");
+                                  if (value!.isEmpty) {
+                                    return "Please enter your phone number";
+                                  } else if (!regex.hasMatch(value)) {
+                                    return "Please enter a valid phone number";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "Phone Number",
+                                  hintStyle:
+                                      Theme.of(context).textTheme.bodyText1,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  errorBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Colors.red,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.03,
-                          ),
-                          IconButton(
-                            iconSize: MediaQuery.of(context).size.width * 0.1,
-                            icon: const Icon(Icons.send_rounded),
-                            onPressed: () async {
-                              await registerUser(
-                                  _phoneNumberController.text
-                                      .replaceAll(" ", ""),
-                                  context);
-                            },
-                          ),
-                        ],
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.03,
+                            ),
+                            IconButton(
+                              iconSize: MediaQuery.of(context).size.width * 0.1,
+                              icon: const Icon(Icons.send_rounded),
+                              onPressed: () async {
+                                if (_phoneFormKey.currentState!.validate()) {
+                                  await registerUser(
+                                      _phoneNumberController.text
+                                          .replaceAll(" ", ""),
+                                      context);
+                                }
+                              },
+                            ),
+                          ],
+                        ),
                       )
                     ],
                   )
